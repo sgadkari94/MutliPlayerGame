044ba9e47a0b1388e3de3211a00ce22bfa863664
priyaStevens <pgupta14@stevens.edu>
2020-04-08 13:32:35 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,21 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const exphbs  = require('express-handlebars');
+//const configRoutes = require("./routes");
+
+ app.use(express.static(path.join(__dirname, '/public')));
+ app.use(bodyParser.urlencoded({ extended: true }));
+
+// app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+// app.set('view engine', 'handlebars');
+
+
+
+//configRoutes(app);
+
+app.listen(3000, () => {
+	console.log("We've now got a server!");
+	console.log('Your routes will be running on http://localhost:3000');
+});
