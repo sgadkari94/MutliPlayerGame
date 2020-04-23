cedddf36849372da4a316ed8abf8fdf6dffcf347
Sanket Gadkari <61213614+sgadkari94@users.noreply.github.com>
2020-04-23 04:31:04 -0400

    Added randomized room number generation
    2nd log for today.
    Added randomized room number generation.
    socket.rooms[Object.keys(socket.rooms)[0]]- important code

--- app.js
+++ app.js
@@ -16,19 +16,67 @@ http.listen(port,function(){
     
 });
 
-var roomno=0;
-io.sockets.on('connection',function(socket){
-    console.log('a new client connected')
+var roomno=Math.floor(Math.random() * 100000);;
+
+var roomnoarray=[];
+
+function generateroom(){
     
+        var newno = Math.floor(Math.random() * 100000);
+        roomnoarray.push(newno)
+   
+
+}
+
+io.sockets.on('connection',function(socket){
+    
+    console.log('a new client connected')
     if(io.nsps['/'].adapter.rooms[roomno] && io.nsps['/'].adapter.rooms[roomno].length > 1)
-    {roomno++;} 
+    {roomno=Math.floor(Math.random() * 100000);} 
     socket.join(roomno);
-    io.sockets.in(roomno).emit('connectToRoom', roomno);
+   io.sockets.in(roomno).emit('connectToRoom', roomno);
+   console.log(io.nsps['/'].adapter.rooms)
+    
+    
+    /* 
+       // generateroom();
+        console.log('1st room created *')
+        console.log(io.nsps['/'].adapter.rooms)
+    if  (io.nsps['/'].adapter.rooms.length==0){
+        generateroom();
+        var number =roomnoarray.length;
+        socket.join(roomnoarray[number-1]);
+        console.log('1st room created')
+        console.log('socket joined room'+roomnoarray[number-1])
+    }
+    else if (io.nsps['/'].adapter.rooms[roomnoarray[roomnoarray.length-1]]&&io.nsps['/'].adapter.rooms[roomnoarray[roomnoarray.length-1]].length>=1)
+    {
+        generateroom();
+        var number =roomnoarray.length;
+        socket.join(roomnoarray[number-1]);
+        console.log('socket joined room'+roomnoarray[number-1])
+    }
+    else
+    {
+        var number =roomnoarray.length;
+        socket.join(roomnoarray[number-1]);
+        console.log('socket joined room'+roomnoarray[number-1])
+    } */
     
     socket.on('sendmsg',function(data){
-         
-        io.sockets.in('0').emit('updateHeader',data)
+       
+        
+        console.log(socket.rooms[Object.keys(socket.rooms)[0]])
+        //console.log(io.sockets.manager.roomClients[socket.id])
+        //io.sockets.in(socket.rooms).emit('updateHeader',data)
+        //socket.broadcast.to(socket.rooms).emit('updateHeader',data)
+        io.sockets.in(socket.rooms[Object.keys(socket.rooms)[0]]).emit('updateHeader',data)
+        for (let index = 0; index < roomnoarray.length; index++) {
+            
+            io.sockets.in("'"+roomnoarray[index]+"'").emit('updateHeader',data)
+        }
         
     })
 })
+
 
