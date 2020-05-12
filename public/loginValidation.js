b9e43217869a4e6e72c8a7e5d7a3d2359099e0be
priyaStevens <pgupta14@stevens.edu>
2020-05-11 22:32:46 -0400

    login Validation

--- public/loginValidation.js
+++ public/loginValidation.js
@@ -1,16 +1,17 @@
-const name = document.getElementById("userName")
-const pass = document.getElementById("password")
-const form = document.getElementById("loginForm")
 
-form.addEventListener('submit', (e) =>{
+function loginVal(e){
+    const name = document.getElementById("userName");
+    const pass = document.getElementById("password");
+    const form = document.getElementById("loginForm");
     if(name.value.length < 4 ){
         alert("Error: Name must be atleast 4 characters long")
-        e.preventDefault()
+       
     }
     
     else if(pass.value.length == 0){
         alert("Error: Please enter the password")
-        e.preventDefault()
     }
-})
+
+    e.preventDefault();
+}
 
